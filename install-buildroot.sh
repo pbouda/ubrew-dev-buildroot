e443a928c3f627b9c8e47810e27a6c3c508f8967
Peter Bouda <pbouda@cidles.eu>
2015-06-24 10:11:29 +0100

    Added gamepad patch for qtbase.

--- install-buildroot.sh
+++ install-buildroot.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "Downloading Buildroot..."
 cd buildroot-qt-dev
 wget http://buildroot.net/downloads/buildroot-2015.05.tar.bz2
 tar xjvf buildroot-2015.05.tar.bz2
 
+echo "Configuring..."
 cd buildroot-2015.05
 make defconfig BR2_DEFCONFIG=../config/buildroot-raspi2.conf
+
+echo "Installing gamepad patch to qtbase..."
+install package/qt5/qt5base/0008-gamepad_device.patch $BUILDROOT_PATH/package/qt5/qt5base/
+
+echo "Starting build, this may take a while..."
 make
 
 cd ../..
